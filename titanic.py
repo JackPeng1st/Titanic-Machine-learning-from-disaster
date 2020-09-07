b8427a54cdb65a058aa753b5f3b94172fc67b74e
JackPeng1st <45029008+JackPeng1st@users.noreply.github.com>
2020-09-07 17:56:50 +0800

    Update titanic.py

--- titanic.py
+++ titanic.py
@@ -1,8 +1,10 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import metrics, cross_validation
+
 #with open('C:/Users/jackp/OneDrive/文件/專題/titanic/train.csv', newline='') as csvfile:
    # rows = csv.reader(csvfile)
     #for row in rows:
@@ -16,6 +18,13 @@ print(train_data.describe())
 print(train_data.isnull().sum())  
 print(test_data.isnull().sum())
 
+#survival number
+labels='Survived','dead'
+size=(len(train_data[train_data['Survived']==1]),len(train_data[train_data['Survived']==0]))
+plt.pie(size , labels = labels,autopct='%1.1f%%')
+plt.axis('equal')
+plt.show()
+
 #women survival rate (train data)
 women = train_data.loc[train_data.Sex == 'female']["Survived"]
 survival_rate_women = sum(women)/len(women)
@@ -23,8 +32,15 @@ print(survival_rate_women)
 
 men=train_data.loc[train_data.Sex=='male']["Survived"]
 survival_rate_men=sum(men)/len(men)
-print(survival_rate_men)
+print(survival_rate_men,survival_rate_women)
 
+##bar chart
+x = ['Man survival rate','Woman survival rate']
+y=[survival_rate_men,survival_rate_women]
+plt.bar(x, y)
+plt.grid(True)
+plt.show()
+####
 def bar_chart(feature):
     survived = train_data[train_data['Survived']==1][feature].value_counts()
     dead = train_data[train_data['Survived']==0][feature].value_counts()
@@ -49,8 +65,7 @@ bar_chart('Pclass')
 bar_chart('Parch')
 
 #####feature engineering
-# Scikit-learn 需要train dataset及label dataset（即答案）各一
-Y_label= train_data.Survived 
+
 
 ###將test_data和train_data合併一起處理
 data_all=train_data.append(test_data)
@@ -89,6 +104,8 @@ data_all.info()
 data_all.drop('Survived',1,inplace=True)
 X_train=data_all[0:len(train_data)]
 X_test=data_all.iloc[len(train_data):]
+# Scikit-learn 需要train dataset及label dataset（即答案）各一
+Y_label= train_data.Survived 
 
 #Y_pred = cross_validation.cross_val_predict(RandomForestClassifier(n_estimators=100), X_train, Y_label, cv=10)
 #acc_random_forest = metrics.accuracy_score(Y_label, Y_pred)
